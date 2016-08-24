54ad43c528283c31677e21637c36143c2da4f316
Dennis Stephen “Heavy D” Lebhardt Jr. <dennislebhardt79@gmail.com>
2016-08-24 05:35:47 +0200

    Revert "roomservice: upgrade to the needs of modern era"
    This reverts commit 69043d9a9c088d6d0c075f3268deaa3651f91d94.
    
    Change-Id: Iba77af3eae463a1c70e0709e0eb314e078c2b273

--- envsetup.sh
+++ envsetup.sh
@@ -619,18 +619,17 @@ function lunch()
     export TARGET_BUILD_APPS=
 
     local product=$(echo -n $selection | sed -e "s/-.*$//")
-    local device=$(echo -n $product | sed -e "s/.*candy_//")
     check_product $product
     if [ $? -ne 0 ]
     then
         # if we can't find a product, try to grab it off the CANDY github
         T=$(gettop)
         pushd $T > /dev/null
-        build/tools/roomservice.py $device
+        build/tools/roomservice.py $product
         popd > /dev/null
         check_product $product
     else
-        build/tools/roomservice.py -d $device
+        build/tools/roomservice.py $product true
     fi
     if [ $? -ne 0 ]
     then
